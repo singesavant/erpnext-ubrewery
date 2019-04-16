33305e8fd430361651e9976de709ed4bc99d14b3
Guillaume Libersat <glibersat@sigill.org>
2019-04-16 15:39:42 +0200

    rename

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 Micro brewery utilities
 
+
+#### Model customization
+
+InternalError: (1054, u"Unknown column 'warehouse.is_scrap' in 'where clause'") : CHECK
+AttributeError: 'Item' object has no attribute 'abv' : DATA
+
 #### License
 
 GPL
--- ubrewery/u_brewery/report/accises/accises.py
+++ ubrewery/u_brewery/report/accises/accises.py
@@ -11,8 +11,8 @@ from collections import defaultdict
 
 from erpnext.stock.get_item_details import (
         get_conversion_factor,
-        get_basic_details
-)
+        get_basic_details)
+
 
 def get_conditions(filters, table=None):
         conditions = ""
@@ -97,7 +97,6 @@ def resolve_item_name(item):
         return item_name
 
 def execute(filters=None):
-
         columns = [
                 _("Item") + ":Link/Item:200",
                 _("ABV") + "::50",
@@ -109,6 +108,8 @@ def execute(filters=None):
                 _("Liters tasting") + "::100"
         ]
 
+
+
         data = []
 
 
@@ -119,11 +120,11 @@ def execute(filters=None):
                                                'scrapped': 0})
 
         # Calculate manufactured quantity in liters
-        for sold_item in get_manufactured_items(filters):
-                item_doc = frappe.get_doc("Item", sold_item.item_code)
+        for manuf_item in get_manufactured_items(filters):
+                item_doc = frappe.get_doc("Item", manuf_item.item_code)
                 if item_doc.abv:
                         item_name = resolve_item_name(item_doc)
-                        liters = convert_to_liters(item_doc, sold_item.qty)
+                        liters = convert_to_liters(item_doc, manuf_item.qty)
                         if liters >= 0:
                                 qty_per_product[(item_name, item_doc.abv)]['manufactured'] += liters
                         else:
@@ -157,5 +158,6 @@ def execute(filters=None):
                              qtties['sold'],
                              qtties['returned'],
                              qtties['scrapped']])
+
 
         return columns, data
